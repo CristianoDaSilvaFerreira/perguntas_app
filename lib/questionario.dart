dace849026f1882516b4a4859c99dc1c5b095209
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-02-08 23:16:47 -0300

    Update questionario.dart

--- lib/questionario.dart
+++ lib/questionario.dart
@@ -5,7 +5,7 @@ import './resposta.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() quantoResponder;
+  final void Function(int) quantoResponder;
 
   Questionario({
     @required this.perguntas,
@@ -16,11 +16,10 @@ class Questionario extends StatelessWidget {
   // Metodo para verificar se tem uma pergunta selecionada
   bool get temPerguntaSelecionada {
     return perguntaSelecionada < perguntas.length;
-  } 
+  }
 
   @override
   Widget build(BuildContext context) {
-
     // Metodo para pegar a resposta selecioanda
     List<Map<String, Object>> respostas = temPerguntaSelecionada
         ? perguntas[perguntaSelecionada]['respostas']
@@ -29,9 +28,12 @@ class Questionario extends StatelessWidget {
     return Column(
       children: <Widget>[
         Questao(perguntas[perguntaSelecionada]['texto']),
-        ...respostas
-          .map((resp) => Resposta(resp['texto'], quantoResponder)
-          ).toList(),
+        ...respostas.map((resp) {
+          return Resposta(
+            resp['texto'],
+            () => quantoResponder(resp['pontuacao']),
+          );
+        }).toList(),
       ],
     );
   }
